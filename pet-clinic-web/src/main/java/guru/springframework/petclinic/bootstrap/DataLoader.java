a48f0b7ec2aa807de770a0cc6b01be78653eeb2c
Mark Galang <galangmarkg@gmail.com>
2020-12-03 16:25:37 -0500

    Remove unused imports

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import guru.springframework.petclinic.model.Owner;
 import guru.springframework.petclinic.model.Vet;
 import guru.springframework.petclinic.services.OwnerService;
 import guru.springframework.petclinic.services.VetService;
-import guru.springframework.petclinic.services.map.OwnerServiceMap;
-import guru.springframework.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
